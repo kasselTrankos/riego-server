af6b807d6c371da8723a54f1b32a511ffc5c688c
kasselTrankos <alvaro.touzon@gmail.com>
2019-05-12 19:37:05 +0200

    add router and httaccess

--- index.php
+++ index.php
@@ -1,4 +1,16 @@
 <?php
-echo "HOLA MUNDO";
-phpinfo();
-?>
+  use Phroute\Phroute\RouteCollector;
+
+  $router = new RouteCollector();
+  
+  $router->get('/api/riegos', function(){
+    return '<joson>Alovaro is ok</joson>';
+  });
+  
+  $router->post('/example/{id}', function($id){
+    return 'This route responds to requests with the POST method at the path /example/1234. It passes in the parameter as a function argument.';
+  });
+  
+  $router->any('/example', function(){
+    return 'This route responds to any method (POST, GET, DELETE, OPTIONS, HEAD etc...) at the path /example';
+  });
