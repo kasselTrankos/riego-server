15801c3c5d455101ed5b2b587ff0cb6b12cd954a
kasselTrankos <alvaro.touzon@gmail.com>
2019-05-13 21:54:01 +0200

    set tpl for volumen

---
+++ cache/6a/6a6610dffc4445108c1bf669c9fd5c9923d085e5bd75ba210df5b9ab5fdb85f5.php
@@ -0,0 +1,87 @@
+<?php
+
+use Twig\Environment;
+use Twig\Error\LoaderError;
+use Twig\Error\RuntimeError;
+use Twig\Extension\SandboxExtension;
+use Twig\Markup;
+use Twig\Sandbox\SecurityError;
+use Twig\Sandbox\SecurityNotAllowedTagError;
+use Twig\Sandbox\SecurityNotAllowedFilterError;
+use Twig\Sandbox\SecurityNotAllowedFunctionError;
+use Twig\Source;
+use Twig\Template;
+
+/* api.html */
+class __TwigTemplate_c488567c3030e8b49ee17e2070d4e22e3a9c196bb36dc8f63c0aab09a22d6f54 extends \Twig\Template
+{
+    private $source;
+
+    public function __construct(Environment $env)
+    {
+        parent::__construct($env);
+
+        $this->source = $this->getSourceContext();
+
+        $this->parent = false;
+
+        $this->blocks = [
+        ];
+    }
+
+    protected function doDisplay(array $context, array $blocks = [])
+    {
+        // line 1
+        echo "<!DOCTYPE html>
+<html>
+  <head>
+    <title>Api</title>
+    <!-- Latest compiled and minified CSS -->
+    <link rel=\"stylesheet\" href=\"https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css\" integrity=\"sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u\" crossorigin=\"anonymous\">
+    <style>
+      h1, h2, h3, h4, h5, h6 {
+        text-align: center;
+      }
+    </style>
+  </head>
+  <body>
+    <div class=\"container-fluid\">
+      <div class=\"row\">
+        <div class=\"col-md-6 col-md-offset-3\">
+          <h1>";
+        // line 17
+        echo twig_escape_filter($this->env, ($context["title"] ?? null), "html", null, true);
+        echo "</h1>
+          <p>
+            ";
+        // line 19
+        echo twig_escape_filter($this->env, ($context["body"] ?? null), "html", null, true);
+        echo "
+          </p>
+        </div>
+      </div>
+    </div>
+  </body>
+</html>";
+    }
+
+    public function getTemplateName()
+    {
+        return "api.html";
+    }
+
+    public function isTraitable()
+    {
+        return false;
+    }
+
+    public function getDebugInfo()
+    {
+        return array (  58 => 19,  53 => 17,  35 => 1,);
+    }
+
+    public function getSourceContext()
+    {
+        return new Source("", "api.html", "/var/www/html/riego/server/tpl/api.html");
+    }
+}
--- routes/Api.php
+++ routes/Api.php
@@ -1,19 +1,31 @@
 <?php
   require_once __DIR__.'/../vendor/autoload.php';
-  $loader = new \Twig\Loader\FilesystemLoader(__DIR__.'/../tpl');
-  $twig = new \Twig\Environment($loader, [
-    'cache' => __DIR__.'/../cache',
-  ]);
-
+  
   class Api {
+    private $TWIG;
+    public function __construct() {
+      $loader = new \Twig\Loader\FilesystemLoader(__DIR__.'/../tpl');
+      $this->TWIG = new \Twig\Environment($loader, [
+        'cache' => __DIR__.'/../cache',
+        ]);
+      $this->TWIG->getExtension(\Twig\Extension\CoreExtension::class)
+        ->setTimezone('Europe/Madrid');
+    }
     function index() {
-      echo " give a minenen";
+      $template = $this->TWIG->load('api.html');
+      echo $template->render(
+        ['title' => 'Riego application server',
+        'body'=>'This is an application to mantain at date the riego information of my home.']);
     }
     function api() {
-      echo " api is here";
+      echo "this is api";
     }
     function volumen() {
-      echo "riegos api";
+      $template = $this->TWIG->load('volumen.json');
+      header('Content-Type: application/json');
+      echo $template->render([
+        "volumen"=>100
+      ]);
     }
 
   }
